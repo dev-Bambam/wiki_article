fee392178eb375e9ef9294b8a53a7e333ee8e3ec
dev_Bambam <ayogood18@gmail.com>
2026-05-28 12:12:56 +0100

    feat: done with this app

--- lib/main.dart
+++ lib/main.dart
@@ -67,7 +67,7 @@ class ArticleView extends StatefulWidget {
 }
 
 class _ArticleViewState extends State<ArticleView> {
-  final ArticleViewModel viewModel = ArticleViewModel(ArticleModel());
+  final viewModel = ArticleViewModel(ArticleModel());
 
   @override
   void initState() {
@@ -91,11 +91,68 @@ class _ArticleViewState extends State<ArticleView> {
             (_, _, final Exception e) => Text('Error: $e'),
             (_, final summary?, _) => ArticlePage(
               summary: summary,
-              nextArticleCallback: viewModel.fetchArticle(),
+              nextArticleCallback: viewModel.fetchArticle,
             ),
             _ => const Text('Something went wrong'),
           };
         },
+      ),
+    );
+  }
+}
+
+class ArticlePage extends StatelessWidget {
+  const ArticlePage({
+    super.key,
+    required this.summary,
+    required this.nextArticleCallback,
+  });
+
+  final Summary summary;
+  final VoidCallback nextArticleCallback;
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          ArticleWidget(summary: summary,),
+          ElevatedButton(
+            onPressed: nextArticleCallback,
+            child: Text('Next random article'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ArticleWidget extends StatelessWidget {
+  const ArticleWidget({super.key, required this.summary});
+
+  final Summary summary;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: Column(
+        spacing: 10,
+        children: [
+          if (summary.hasImage) Image.network(summary.originalImage!.source),
+          Text(
+            summary.titles.normalized,
+            overflow: TextOverflow.ellipsis,
+            style: Theme.of(context).textTheme.displaySmall,
+          ),
+          if (summary.description != null)
+            Text(
+              summary.description!,
+              overflow: TextOverflow.ellipsis,
+              style: Theme.of(context).textTheme.bodySmall,
+            ),
+          Text(summary.extract),
+        ],
       ),
     );
   }
